a01569ce8c2c93bdf3e0e7eac4c498fc16208c2b
Tatiana Pope <tatianapope13@gmail.com>
2021-05-07 09:41:35 -0400

    Update - Part 4 5/7

--- Tatiana_PersonalityQuiz/QuestionViewController.swift
+++ Tatiana_PersonalityQuiz/QuestionViewController.swift
@@ -43,6 +43,40 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var questionProgressView: UIProgressView!
     
     
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    
+    @IBOutlet weak var multiSwitch4: UISwitch!
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        
+        let currentAnswers = questions[questionIndex].answers
+        switch sender { case singleButton1: answersChosen.append(currentAnswers[0])
+        case singleButton2: answersChosen.append(currentAnswers[1])
+        case singleButton3: answersChosen.append(currentAnswers[2])
+        case singleButton4: answersChosen.append(currentAnswers[3])
+        default: break
+        }
+        nextQuestion()
+        
+    }
+    
+    @IBAction func multipleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +84,10 @@ class QuestionViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    var answersChosen: [Answer] = []
+    
+    var questionIndex = 0
     
     func updateUI() {
     singleStackView.isHidden = true
@@ -62,13 +100,42 @@ class QuestionViewController: UIViewController {
     questionLabel.text = currentQuestion.text
     questionProgressView.setProgress(totalProgress, animated: true)
 
-    switch currentQuestion.type {
-    case .single:
-    singleStackView.isHidden = false
-    case .multiple: multipleStackView.isHidden = false
-    case .ranged: rangedStackView.isHidden = false }
+        switch currentQuestion.type {
+        case .single: updateSingleStack(using: currentAnswers)
+        case.multiple: updateMultipleStack(using: currentAnswers)
+        case .ranged: updateRangedStack(using: currentAnswers) }
+        
     }
     
+    func updateSingleStack(using answers: [Answer]) {
+    singleStackView.isHidden = false
+    singleButton1.setTitle(answers[0].text, for: .normal)
+    singleButton2.setTitle(answers[1].text, for: .normal)
+    singleButton3.setTitle(answers[2].text, for: .normal)
+    singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+    multipleStackView.isHidden = false
+    multiLabel1.text = answers[0].text
+    multiLabel2.text = answers[1].text
+    multiLabel3.text = answers[2].text
+    multiLabel4.text = answers[3].text }
+    
+    func updateRangedStack(using answers: [Answer]) {
+    rangedStackView.isHidden = false
+    rangedLabel1.text = answers.first?.text
+    rangedLabel2.text = answers.last?.text
+    }
+    
+    func nextQuestion() {
+        questionIndex += 1
+            if questionIndex < questions.count {
+        updateUI()
+        } else {
+    performSegue(withIdentifier: "Results", sender: nil)
+    }
+    }
         
 
     /*
@@ -83,6 +150,6 @@ class QuestionViewController: UIViewController {
     
     var questions: [Question] = [ Question( text: "Which food do you like the most?", type: .single, answers: [ Answer(text: "Steak", type: .dog), Answer(text: "Fish", type: .cat),Answer(text: "Carrots", type: .rabbit), Answer(text: "Corn", type: .turtle) ] ),Question( text: "Which activities do you enjoy?", type: .multiple, answers: [Answer(text: "Swimming", type: .turtle), Answer(text: "Sleeping", type: .cat),Answer(text: "Cuddling", type: .rabbit), Answer(text: "Eating", type: .dog) ] ),Question( text: "How much do you enjoy car rides?", type: .ranged, answers: [Answer(text: "I dislike them", type: .cat), Answer(text: "I get a little nervous", type: .rabbit), Answer(text: "I barely notice them", type: .turtle), Answer(text: "I love them", type: .dog) ] ) ]
     
-    var questionIndex = 0
+    
 
 }
